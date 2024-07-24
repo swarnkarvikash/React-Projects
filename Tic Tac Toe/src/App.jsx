a7e09a001e85a17716e152d995affbc495ac852a
swarnkarvikash <vikashswarnkar3@gmail.com>
2024-07-24 13:18:45 +0530

    Initializa Tic Tac Toe app

---
+++ Tic Tac Toe/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Grid from './components/Grid/Grid';
+
+function App() {
+
+
+  return (
+    <>
+    <Grid numberOfCards={9} />
+    </>
+  )
+}
+
+export default App
